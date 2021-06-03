c5e39d232be610b085849bd1f1b42984d54fbd6b
Arvind Mundhe <abmundhe_b19@it.vjti.ac.in>
2021-06-03 14:28:18 +0530

    Late update
    Late update

---
+++ Sales_by_Match.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d", x)
+#define pl(x) printf("%lld", x)
+#define ps(s) printf("%s", s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+void solve()
+{
+    int a, b, n, m, elmt, ans = 0, flag;
+    cin >> n;
+    int arr[n];
+    int hash[100] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        cin >> elmt;
+        arr[i] = elmt;
+        hash[elmt-1]++;
+    }
+    for (int i = 0; i < 100; i++)
+    {
+        ans += hash[i] / 2;
+
+    }
+    cout << ans;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+int pow(int base, int exp)
+{
+    int mod = 10;
+    base %= mod;
+    int result = 1;
+    while (exp > 0)
+    {
+        if (exp & 1)
+            result = ((ll)result * base) % mod;
+        base = ((ll)base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+};
